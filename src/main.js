305a308666571cbe33c5278fdbdcafc93bde7e7a
Mor1aty <971896352@qq.com>
2020-02-25 22:06:35 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入全局样式
+import './assets/css/global.css'
+// 导入 axios
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
